4c9c36fe539c6b68f7a1553b0c850c4bbff96612
Artūras Danielius <arturas.danielius@gmail.com>
2022-06-08 16:42:48 +0300

    pirmas

--- js/uzduotys.js
+++ js/uzduotys.js
@@ -1,14 +1,6 @@
 /*Kintamųjų inicijavimas
 
-
-Sukurti 3 kintamuosius su teksto tipo reikšmėmis
-Po kiekvieno jų inicijavimo, išvesti į console
-Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
-Po kiekvieno jų inicijavimo, išvesti į console
-Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
-Po kiekvieno jų inicijavimo, išvesti į console*/
-
-1/*Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+1. /*Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
 Po kiekvieno jų inicijavimo, išvesti į console*/
 
 let a = 4;
@@ -17,8 +9,41 @@ console.log(a);
 let b = 9;
 console.log(b);
 
-let c = 11
+let c = 11;
 console.log(c);
 
-let pinigine = 1;
-console.log(username, 'pinigineje turi', pinigine, 'pinigu');
+2. /*Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console*/
+
+let n1 = 'Labas';
+console.log(n1);
+
+let n2 = 'Rytas';
+console.log(n2);
+
+let n3 = 'Lietuva';
+console.log(n3);
+
+3. /*Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console*/
+
+let sk1 = [1, 2, 3, 4, 5]
+console.log(sk1);
+
+let sk2 = [5, 4, 3, 2, 1]
+console.log(sk2);
+
+let sk3 = [6, 5, 7, 4, 8]
+console.log(sk3);
+
+4. /*Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console*/
+
+let tx1 = ['as', 'tu', 'mes', 'visi', 'kartu']
+console.log(tx1);
+
+let tx2 = ['5', '4', '3', '2', '1']
+console.log(tx2);
+
+let tx3 = ['q', 'w', 'e', 'r', 't']
+console.log(tx3);
