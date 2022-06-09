292b718c66c863ed2b9f0e217f3da673d2b4379e
Artūras Danielius <arturas.danielius@gmail.com>
2022-06-10 01:21:40 +0300

    veiksmai su kintamaisiais

--- js/uzduotys.js
+++ js/uzduotys.js
@@ -3,13 +3,13 @@ console.log('Kintamųjų inicijavimas');
 1. /*Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
 Po kiekvieno jų inicijavimo, išvesti į console*/
 
-let a = 4;
+const a = 4;
 console.log(a);
 
-let b = 9;
+const b = 9;
 console.log(b);
 
-let c = 11;
+const c = 11;
 console.log(c);
 
 2. /*Sukurti 3 kintamuosius su teksto tipo reikšmėmis
@@ -52,14 +52,34 @@ console.log('---------------------------------------');
 
 console.log('Veiksmai su kintamaisiais');
 
-
-/*Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
-Rezultatą išvesti į console
-Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
-1-2+3-4+5
-Rezultatą išvesti į console
-Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas*/
-
-
 1. /*Susumuoti visus skaičiaus tipo kintamuosius
 Rezultatą išvesti į console*/
+
+console.log(a, '+', b, '+', c, '=', a + b + c)
+
+2. /*Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+Rezultatą išvesti į console*/
+
+const nsuma = n1 + n2 + n3
+console.log(n1, n2, n3);
+
+3. /*Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+1-2+3-4+5
+Rezultatą išvesti į console*/
+
+console.log('1-2+3-4+5', '=', 1 - 2 + 3 - 4 + 5);
+
+
+const marks = [1, 2, 3, 4, 5]
+const total = marks[0] - marks[1] + marks[2] - marks[3] + marks[4];
+console.log(total);
+
+const marks1 = [5, 4, 3, 2, 1]
+const total1 = marks1[0] - marks1[1] + marks1[2] - marks1[3] + marks1[4];
+console.log(total1);
+
+const marks2 = [6, 5, 7, 4, 8]
+const total2 = marks2[0] - marks2[1] + marks2[2] - marks2[3] + marks2[4];
+console.log(total2);
+
+4. /*Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas*/
