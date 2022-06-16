ca510061bedf5754f701b0f8bfa55bda7d79a026
Artūras Danielius <arturas.danielius@gmail.com>
2022-06-16 14:05:30 +0300

    Refactoring: Suma with parsing

--- js/number-parsing.js
+++ js/number-parsing.js
@@ -1,27 +1,15 @@
-console.clear();
-
-const n1 = '77';
-const n2 = 5;
-
 function suma(a, b) {
-    const initialAsize = ('' + a).length;
-    const initialBsize = ('' + b).length;
+    a = +a;
+    b = +b;
 
-    a = parseInt(a);
-    b = parseInt(b);
-
-    const afterAsize = ('' + a).length;
-    const afterBsize = ('' + b).length;
-
-    if (isNaN(a) || initialAsize !== afterAsize) {
+    if (isNaN(a)) {
         return 'ERROR: Pirmas parametras yra ne tikras skaicius';
     }
-    if (isNaN(b) || initialBsize !== afterBsize) {
+    if (isNaN(b)) {
         return 'ERROR: Antras parametras yra ne tikras skaicius';
     }
 
     return a + b;
 }
 
-const ats = suma(n1, n2);
-console.log(ats);
+module.exports = suma;
